48a178aea2ac1d1ee9a243f625953c63e792d833
Ramiro Pérez <fpaz.datalytics@supervielle.com.ar>
2023-01-28 16:11:42 -0300

    load key

--- python_code/main.py
+++ python_code/main.py
@@ -1,6 +1,6 @@
 from get_data_api import country_, compe_, teams_, standing_
 from db_insert import *
-
+import time
 if __name__ == '__main__':
     schema_name = 'dbo'
     list_functions = [country_, compe_, teams_, standing_]
@@ -10,7 +10,9 @@ if __name__ == '__main__':
         create_table_s = schema_create_table(build_schema_)
         insert_table_s = schema_insert_table(build_schema_)
         create_table(build_schema_, schema_name, f'{function.__name__}'.rstrip('_'))
+        time.sleep(15)
         insert_data_table(build_schema_, schema_name, f'{function.__name__}'.rstrip('_'), df_dataframe(function.__name__))
+        time.sleep(50)
 
         # build_schema_countries = build_schema(df_dataframe(country_))
         # create_table_s = schema_create_table(build_schema_countries)
